d3ba6482903d5a793738b5eaefdac0beae689571
Abhay1918 <97477834+Abhay1918@users.noreply.github.com>
2022-01-11 02:18:44 +0530

    Add files via upload

---
+++ asg1_Output.py
@@ -0,0 +1,41 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:\Users\lenovo\Documents\asg1_21105108.py =============
+Write a Python program to find average of three numbers entered by the user. 
+
+enter three numbders whose avg is to be calculated: 70
+10
+66
+Result:  48.666666666666664
+
+Write a python program to compute a person's income tax.
+
+Enter your gross income: 1200000
+No. of Dependents: 3
+Total income tax you have to pay:  236200.0
+
+Write a python program to store different data type values into a list.
+
+['SID', 'Name', 'Gender', 'Course', 'CGPA']
+[21105108, 'Abhay Sharma', 'M', 'ECE', 8.0]
+[21105066, 'vishvjeet', 'M', 'ECE', 7.5]
+[21105093, 'Akshat', 'M', 'ECE', 8.5]
+
+Write a python program to enter marks of 5 students into a list and display them in sorted manner.
+
+Marks of the student
+Before sorted:  [67, 91, 88, 32, 77, 95, 91]
+After sorting:  [32, 67, 77, 88, 91, 91, 95]
+
+Write a Python program to print a specified list after removing 4th element.
+
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+After removing 4th element
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+Remove ‘Black’ and ‘Pink’ from the list and replace them with ‘Purple’. Do that in one line code.
+Remove Black and Pink from the list and replace them with Purple. Do that in one line code.
+
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+After replacing Black and Pink
+['Red', 'Green', 'White', 'purple', 'Yellow']
