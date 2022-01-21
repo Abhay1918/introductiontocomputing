5fe41a938ce0e9f2f2f66f96ddba94dac51c132b
Abhay1918 <97477834+Abhay1918@users.noreply.github.com>
2022-01-21 22:38:57 +0530

    Add files via upload

---
+++ asg2_output 21105108.py
@@ -0,0 +1,47 @@
+Python 3.11.0a3 (main, Dec  8 2021, 22:56:33) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:/Users/abhay/OneDrive/Documents/asg2_inp_21105108abh.py ======
+Question1
+
+part a
+Length of input string = 34
+
+part b
+egaugnal evtisnes esac a si nohtyP
+
+part c
+a case sensitve 
+
+part d
+new_string
+
+part e
+10
+
+part f
+Pythonisacasesensitvelanguage
+Question2
+Hey, Abhay Sharma here! 
+My SID is 21105108 
+I am from ECE and my CGPA is 10
+Question3
+ a&b :  8
+ a|b :  58
+ a^b :  50
+a<<2 :  224 	b<<2 : 40
+a>>2 :  14 	b>>2 : 0
+Question4
+Enter 1st no. : 3 
+Enter 2nd no. : 5
+Enter 3rd no. : 2
+Highest no. = 5
+Question5
+Enter input string: hello name
+Yes
+Question6
+Enter 1st length : 33
+Enter 2nd length : 13
+Enter 3rd length : 5
+NO
+
