42dfd5b42195debe80ce916766dcc277ae267b0c
Abhay1918 <97477834+Abhay1918@users.noreply.github.com>
2022-02-14 00:33:04 +0530

    Add files via upload

---
+++ ouput_asg3.py
@@ -0,0 +1,140 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:/Users/abhay/OneDrive/Documents/abhay_21105108_input_asg3.py ===
+                                                      ASSIGNMENT 3 
+Question 1 :  To count the number of words in a string or characters . 
+
+Enter a string : I am going to be number 1 this time
+Since word length is greater than 1 so checking for the number of same words :
+i            :1         
+am           :1         
+going        :1         
+to           :1         
+be           :1         
+number       :1         
+1            :1         
+this         :1         
+time         :1         
+
+
+Question 2:- To print the next date of input date .
+
+Enter  year -2050
+
+Enter month -4
+
+Enter day -31
+
+Month you have entered has not  more than 30 days!!!!! 
+Please enter the valid day 
+
+Enter day -24
+
+
+Entered date is : 24/04/2050
+
+Next date is : 25/04/2050
+
+
+
+Question 3: to create a list of tuples having first element as the number and second as the square of the number. 
+
+Enter all the numbers in a single line separated by space : 1 2 3 4 5 6 7 8 9 10
+New list is :
+ [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81), (10, 100)]
+
+
+Question 4: To print the grade and remark according to his/her grade .  
+
+Grade distribution is as :
+
+Letter Grade   Performance         Grade Point    
+A+             outstanding         10             
+A              Excellent           9              
+B+             Very Good           8              
+B              Good                7              
+C+             Average             6              
+C              Below Average       5              
+D              Poor                4              
+
+Enter your grade between 4 to 10 : 17
+Grade must be between 4 to 10 !!!! 
+ please enter a valid grade : 
+
+Enter your grade between 4 to 10 : 2
+Grade must be between 4 to 10 !!!! 
+ please enter a valid grade : 
+
+Enter your grade between 4 to 10 : 10
+
+Your Grade is A+ and outstanding Performance.
+
+
+Question 5: Pattern printing.
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+Question 6 : Dictionary functions and sorting.
+
+Enter name : Abhay Sharma
+Enter  SID  :21105108
+
+Do you want to store more student data (Y/N) : 
+vishvjeet thakur
+
+Please enter Y or N only!!!
+
+
+Do you want to store more student data (Y/N) : 
+Enter name : Jitesh singla
+
+Please enter Y or N only!!!
+
+
+Do you want to store more student data (Y/N) : 
+n
+
+Student data in the order as entered by the user : 
+21105108  :  Abhay Sharma
+
+Sorted dictionary according to student names :
+21105108       :Abhay Sharma   
+
+Sorted dictionary according to student sids :
+21105108  :  Abhay Sharma
+
+Enter the SID whose name you want to search :21105108
+
+Name of the student with SID 21105108 is Abhay Sharma.
+
+
+Question 7 :  Printing of Fibonacci series .
+Enter the number of terms to which you want the Fibonacci Series :21
+
+1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 
+
+Average is  0.09523809523809523
+
+
+Question 8 :  Set methods -  union , intersection , difference .
+Given sets are :
+Set1 =  {1, 2, 3, 4, 5}
+Set2 =  {8, 2, 4, 6}
+Set3 =  {1, 17, 5, 9, 13}
+
+New set of all elements that are in Set1 and Set2 but not both :  {1, 3, 5, 6, 8}
+
+New set of all elements that are in only one of the three sets Set1, Set2 and Set3 : {3, 6, 8, 9, 13, 17}
+
+New set of elements that are exactly two of the sets Set1, Set2 and Set3 :  {1, 2, 4, 5}
+
+New set of all integers in the range 1 to 10 that are not in Set1 : {6, 7, 8, 9, 10}
+
+New set of all integers in the range 1 to 10 that are not in Set1,Set2 and Set3: {10, 7}
